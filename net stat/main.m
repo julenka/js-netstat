443a7e069e78054988d416bab64be9340d299d04
Julia Schwarz <julenka@cs.cmu.edu>
2015-07-27 11:00:11 -0400

    Initial Commit

---
+++ net stat/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  net stat
+//
+//  Created by Julia Schwarz on 7/27/15.
+//  Copyright (c) 2015 Julia Schwarz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
